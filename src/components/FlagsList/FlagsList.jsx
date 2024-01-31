25df11548ce8eeba888b6264612eab0f08f5aa23
codyivy0 <codyivy0@gmail.com>
2024-01-31 15:08:48 -0600

    filter responsive

--- src/components/FlagsList/FlagsList.jsx
+++ src/components/FlagsList/FlagsList.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useTheme } from "../../ThemeContext";
 import styles from "./FlagsList.module.css";
 import PropTypes from "prop-types";
@@ -5,39 +6,82 @@ import PropTypes from "prop-types";
 const FlagsList = ({ countryData }) => {
   const { darkMode } = useTheme();
 
+  const [selectedRegion, setSelectedRegion] = useState("Filter by Region"); // Default to 'All' regions
+
+  const regions = [
+    "Filter by Region",
+    "All",
+    "Asia",
+    "Europe",
+    "Africa",
+    "Americas",
+    "Oceania",
+  ];
+
+  const filteredCountries =
+    selectedRegion === "All" || selectedRegion === "Filter by Region"
+      ? countryData
+      : countryData.filter((country) => country.region === selectedRegion);
+
+  const handleRegionChange = (event) => {
+    setSelectedRegion(event.target.value);
+  };
+
   function formatPopulationWithCommas(population) {
     return population.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
   return (
-    <div className={styles.flagsListContainer}>
-      {countryData.map((country, index) => (
-        <div
-          className={`${styles.countryCard} ${darkMode ? styles.cardDark : ""}`}
-          key={index}
-        >
-          <img src={country.flags.svg} alt="" />
-          
-          <div
-            className={`${styles.countryInfo} ${
-              darkMode ? styles.cardDark : ""
-            } `}
+    <>
+      {" "}
+      <section className={styles.controls}>
+        <input type="text" className={styles.filterInput} placeholder="this doesnt work yet" />
+        <div className={styles.dropdownContainer}>
+          <select
+            id="regionDropdown"
+            className={styles.dropdown}
+            value={selectedRegion}
+            onChange={handleRegionChange}
           >
-            <h3>{country.name}</h3>
-            <p>
-              <span className={styles.bold}>Population:</span>{" "}
-              {formatPopulationWithCommas(country.population)}
-            </p>
-            <p>
-              <span className={styles.bold}>Region:</span> {country.region}
-            </p>
-            <p>
-              <span className={styles.bold}>Capital:</span> {country.capital}
-            </p>
-          </div>
+            {regions.map((region) => (
+              <option key={region} value={region}>
+                {region}
+              </option>
+            ))}
+          </select>
         </div>
-      ))}
-    </div>
+      </section>
+      <div className={styles.flagsListContainer}>
+        {filteredCountries.map((country, index) => (
+          <div
+            className={`${styles.countryCard} ${
+              darkMode ? styles.cardDark : ""
+            }`}
+            key={index}
+          >
+            <img src={country.flags.svg} alt="" />
+
+            <div
+              className={`${styles.countryInfo} ${
+                darkMode ? styles.cardDark : ""
+              } `}
+            >
+              <h3>{country.name}</h3>
+              <p>
+                <span className={styles.bold}>Population:</span>{" "}
+                {formatPopulationWithCommas(country.population)}
+              </p>
+              <p>
+                <span className={styles.bold}>Region:</span> {country.region}
+              </p>
+              <p>
+                <span className={styles.bold}>Capital:</span> {country.capital}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
